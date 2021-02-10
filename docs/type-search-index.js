5e53d0627d34facabcd75d70349163f80d9173b2
Miguel Proba <miguel.proba.estevez@gmail.com>
2021-02-10 18:17:48 +0100

    Segundo commit

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"clases","l":"Circulo"},{"p":"clases","l":"Figura"},{"p":"<Unnamed>","l":"Main"},{"p":"clases","l":"Rectangulo"},{"p":"clases","l":"Triangulo"}]
